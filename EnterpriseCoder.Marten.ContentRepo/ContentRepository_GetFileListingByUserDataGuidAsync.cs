4dc4c6e7ad1b9070f426f7cc1cd37129904b34c6
relledge <russell.elledge@gmail.com>
2025-04-15 07:30:14 -0400

    more documentation updates

---
+++ EnterpriseCoder.Marten.ContentRepo/ContentRepository_GetFileListingByUserDataGuidAsync.cs
@@ -0,0 +1,38 @@
+﻿using EnterpriseCoder.Marten.ContentRepo.Entities;
+using EnterpriseCoder.Marten.ContentRepo.Exceptions;
+using Marten;
+using Marten.Pagination;
+
+namespace EnterpriseCoder.Marten.ContentRepo;
+
+public partial class ContentRepository
+{
+    /// <summary>
+    /// The GetFileListingByUserDataGuidAsync method is used to retrieve a paged listing of resources that were saved
+    /// with a specific "user data" guid.  This can be used as a type of foreign key to a user table in order to
+    /// track quotas if desired.  The guid field associated with the user data is indexed for fast lookup.
+    /// </summary>
+    /// <param name="documentSession">A Marten documentSession that will be used to communicate with the database.</param>
+    /// <param name="bucketName">The name of the bucket to be searched.</param>
+    /// <param name="userGuid">The guid identifier that will be used in the search.</param>
+    /// <param name="oneBasedPage">The one-based page number to be queried.</param>
+    /// <param name="pageSize">The desired size for each page of resource information.</param>
+    /// <returns>Returns a <see cref="PagedContentRepositoryFileInfo"/> that contains the items for the page as well as paging information.</returns>
+    /// <exception cref="BucketNotFoundException">Thrown when the bucket specified by <paramref name="bucketName"/> is not found.</exception>
+    public async Task<PagedContentRepositoryFileInfo> GetFileListingByUserDataGuidAsync(
+        IDocumentSession documentSession,
+        string bucketName, Guid userGuid, int oneBasedPage, int pageSize)
+    {
+        var targetBucket = await _contentBucketProcedures.SelectBucketAsync(documentSession, bucketName);
+        if (targetBucket == null)
+        {
+            throw new BucketNotFoundException(bucketName);
+        }
+
+        IPagedList<ContentFileHeader> pageList =
+            await _fileHeaderProcedures.SelectByUserGuid(documentSession, targetBucket, userGuid, oneBasedPage,
+                pageSize);
+
+        return new PagedContentRepositoryFileInfo(pageList);
+    }
+}
--- EnterpriseCoder.Marten.ContentRepo/IContentRepository.cs
+++ EnterpriseCoder.Marten.ContentRepo/IContentRepository.cs
@@ -155,14 +155,17 @@ public interface IContentRepository
         bool recursive = false);
 
     /// <summary>
-    /// 
+    /// The GetFileListingByUserDataGuidAsync method is used to retrieve a paged listing of resources that were saved
+    /// with a specific "user data" guid.  This can be used as a type of foreign key to a user table in order to
+    /// track quotas if desired.  The guid field associated with the user data is indexed for fast lookup.
     /// </summary>
-    /// <param name="documentSession"></param>
-    /// <param name="bucketName"></param>
-    /// <param name="userGuid"></param>
-    /// <param name="oneBasedPage"></param>
-    /// <param name="pageSize"></param>
-    /// <returns></returns>
+    /// <param name="documentSession">A Marten documentSession that will be used to communicate with the database.</param>
+    /// <param name="bucketName">The name of the bucket to be searched.</param>
+    /// <param name="userGuid">The guid identifier that will be used in the search.</param>
+    /// <param name="oneBasedPage">The one-based page number to be queried.</param>
+    /// <param name="pageSize">The desired size for each page of resource information.</param>
+    /// <returns>Returns a <see cref="PagedContentRepositoryFileInfo"/> that contains the items for the page as well as paging information.</returns>
+    /// <exception cref="BucketNotFoundException">Thrown when the bucket specified by <paramref name="bucketName"/> is not found.</exception>
     Task<PagedContentRepositoryFileInfo> GetFileListingByUserDataGuidAsync(IDocumentSession documentSession,
         string bucketName, Guid userGuid, int oneBasedPage, int pageSize);
 }
