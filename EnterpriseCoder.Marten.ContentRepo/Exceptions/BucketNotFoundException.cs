cd4b35e83b048d563dda27da861c4b70923ed167
relledge <russell.elledge@gmail.com>
2025-05-03 08:49:41 -0400

    documentation for exception classes

--- EnterpriseCoder.Marten.ContentRepo/Exceptions/BucketNotFoundException.cs
+++ EnterpriseCoder.Marten.ContentRepo/Exceptions/BucketNotFoundException.cs
@@ -1,7 +1,14 @@
 ﻿namespace EnterpriseCoder.Marten.ContentRepo.Exceptions;
 
+/// <summary>
+/// Represents an exception thrown when a bucket does not exist.
+/// </summary>
 public class BucketNotFoundException : Exception
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BucketNotFoundException"/> class with the name of the missing bucket.
+    /// </summary>
+    /// <param name="bucketName">The name of the bucket that was not found.</param>
     public BucketNotFoundException(string bucketName) : base(bucketName)
     {
     }
